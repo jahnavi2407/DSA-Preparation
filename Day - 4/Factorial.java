6773dbd3ef82870bfedbb969b08281a431b4431a
jahnavi2407 <118998113+jahnavi2407@users.noreply.github.com>
2024-02-20 22:31:36 +0530

    Input a number and find it's factorial

---
+++ Day - 4/Factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value: ");
+        int n = sc.nextInt();
+        int fac = 1;
+        for (int i=1; i<=n; i++){
+            fac = fac * i;
+        }
+        System.out.println("Factorial of "+n+" is: " +fac);
+    }
+}
